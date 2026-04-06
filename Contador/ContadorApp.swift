0612ce2996b3ffdf9b39a3f96659391848976c65
Álvaro Concha <alvaro.concha.c89@gmail.com>
2026-04-05 21:01:29 -0400

    Initial Commit

---
+++ Contador/ContadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorApp.swift
+//  Contador
+//
+//  Created by Álvaro Concha Carvajal on 05-04-26.
+//
+
+import SwiftUI
+
+@main
+struct ContadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
